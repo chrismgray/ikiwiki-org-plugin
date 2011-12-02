797e8f1930f55d745bba1225d27838f52b9efde1
Chris Gray <chrismgray@gmail.com>
2011-12-02 14:40:31 -0700

    Got rid of supersession warnings.
    There was a problem where emacs would ask if the user wanted to edit
    the input file (even though I wasn't even editing it -- just calling
    narrow-to-region).  This obviously wouldn't work if emacs is being
    used as a daemon, so I needed to get rid of it.

--- lisp/ikiwiki-org-plugin.el
+++ lisp/ikiwiki-org-plugin.el
@@ -17,7 +17,8 @@
 (defun xml-rpc-wait-for-response (buffer)
   (with-current-buffer buffer
     (while (not (re-search-forward "</\\(methodResponse\\|methodCall\\)>" (point-max) t))
-      (revert-buffer t t))))
+      (revert-buffer t t)
+      (narrow-to-region org-ikiwiki-region-start (point-max)))))
 
 (defun xml-rpc-method-call-stdout (method &rest params)
   "Call an XML-RPC method asynchronously at SERVER-URL named METHOD with \
@@ -80,21 +81,32 @@ called with the result as parameter."
   "An alist of the methods that ikiwiki can call.")
 
 (defvar org-ikiwiki-output-file nil)
+(defvar org-ikiwiki-region-start 1)
 
 (defun org-ikiwiki-make-rpc-xml-get-response-fn (input-buffer)
   (lambda ()
     (xml-rpc-wait-for-response input-buffer)
     (let* ((xml-rpc-debug 3)
-	   (xml-list (xml-rpc-request-process-buffer input-buffer))
+	   (xml-list
+	    (with-current-buffer input-buffer
+	     (xml-rpc-clean (xml-parse-region (point-min) (point-max)))))
 	   (method-or-response (car-safe (car-safe xml-list))))
       (with-current-buffer input-buffer
 	(revert-buffer t t)
-	(delete-region (point-min) (point-max))
-	(save-buffer))
+	(setq org-ikiwiki-region-start (point-max))
+	(narrow-to-region (point-max) (point-max)))
       (if (eq method-or-response 'methodResponse)
 	  (let ((valpart (cdr (cdaddr (caddar xml-list)))))
 	    (xml-rpc-xml-list-to-value valpart))
 	(error "Not a methodResponse")))))
+
+(defmacro org-ikiwiki-with-no-supersession-questions (&rest forms)
+  (let ((old-supersession-threat (gensym)))
+   `(let ((,old-supersession-threat (symbol-function 'ask-user-about-supersession-threat)))
+      (fset 'ask-user-about-supersession-threat (lambda (f)))
+      (unwind-protect
+	  (progn ,@forms)
+	(fset 'ask-user-about-supersession-threat ,old-supersession-threat)))))
 
 (defun org-ikiwiki-compile (input-file output-file)
   "Runs the ikiwiki compilation process."
@@ -103,38 +115,39 @@ called with the result as parameter."
 	 (org-ikiwiki-rpc-xml-get-response (org-ikiwiki-make-rpc-xml-get-response-fn input-buffer))
 	 (auto-revert-mode nil))
     (setq org-ikiwiki-output-file output-file)
-    (while (file-exists-p input-file)
-      (xml-rpc-wait-for-response input-buffer)
-      (let* ((xml-rpc-debug 3)
-	     (xml-list (xml-rpc-request-process-buffer input-buffer))
-	     (method-or-response (car-safe (car-safe xml-list))))
-	(with-current-buffer input-buffer
-	  (revert-buffer t t)
-	  (delete-region (point-min) (point-max))
-	  (save-buffer))
-	(cond
-	 ((eq method-or-response 'methodResponse)
-	  (let ((valpart (cdr (cdaddr (caddar xml-list)))))
-	    (xml-rpc-xml-list-to-value valpart)))
-	 ((eq method-or-response 'methodCall)
-	  (let* ((method-name (caddr (caddar xml-list)))
-		 (params (mapcar
-			  (lambda (node)
-			    (xml-rpc-xml-list-to-value
-			     (list node)))
-			  (xml-find-nodes-matching (car xml-list) 'value)))
-		 (method (cadr (assoc method-name methods)))
-		 (result (funcall method org-ikiwiki-rpc-xml-get-response (list->hash params)))
-		 (m-params (list `(param nil ,(car (xml-rpc-value-to-xml-list result)))))
-		 (m-response `((methodResponse nil
-					       ,(append '(params nil) m-params)))))
-	    (with-current-buffer "*scratch*"
-	      (xml-print-with-preamble m-response))
-	    (with-temp-buffer 
-	      (xml-print-with-preamble m-response)
-	      (append-to-file (point-min) (point-max) org-ikiwiki-output-file))))
-	 (t
-	  (error "Not a methodCall or methodResponse")))))
+    (org-ikiwiki-with-no-supersession-questions
+     (while (file-exists-p input-file)
+       (xml-rpc-wait-for-response input-buffer)
+       (let* ((xml-rpc-debug 3)
+	      (xml-list
+	       (with-current-buffer input-buffer
+		 (xml-rpc-clean (xml-parse-region (point-min) (point-max)))))
+	      (method-or-response (car-safe (car-safe xml-list))))
+	 (with-current-buffer input-buffer
+	   (revert-buffer t t)
+	   (setq org-ikiwiki-region-start (point-max))
+	   (narrow-to-region (point-max) (point-max)))
+	 (cond
+	  ((eq method-or-response 'methodResponse)
+	   (let ((valpart (cdr (cdaddr (caddar xml-list)))))
+	     (xml-rpc-xml-list-to-value valpart)))
+	  ((eq method-or-response 'methodCall)
+	   (let* ((method-name (caddr (caddar xml-list)))
+		  (params (mapcar
+			   (lambda (node)
+			     (xml-rpc-xml-list-to-value
+			      (list node)))
+			   (xml-find-nodes-matching (car xml-list) 'value)))
+		  (method (cadr (assoc method-name methods)))
+		  (result (funcall method org-ikiwiki-rpc-xml-get-response (list->hash params)))
+		  (m-params (list `(param nil ,(car (xml-rpc-value-to-xml-list result)))))
+		  (m-response `((methodResponse nil
+						,(append '(params nil) m-params)))))
+	     (with-temp-buffer 
+	       (xml-print-with-preamble m-response)
+	       (append-to-file (point-min) (point-max) org-ikiwiki-output-file))))
+	  (t
+	   (error "Not a methodCall or methodResponse"))))))
     (message "done with that file!")))
 
 (provide 'ikiwiki-org-plugin)
