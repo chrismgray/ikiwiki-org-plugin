e4b00c81d2e2cf39456a3f487801ac0247117e6c
Chris Gray <chrismgray@gmail.com>
2011-12-02 13:16:17 -0700

    Getting closer.

--- lisp/ikiwiki-org-plugin.el
+++ lisp/ikiwiki-org-plugin.el
@@ -1,6 +1,24 @@
 
 (require 'xml-rpc)
 
+(defun xml-find-nodes-matching (node name)
+  "Returns all children of `node' that have an `xml-node-name' equal to `name'."
+  (if (or (eq node '()) (not (listp node)))
+      '()
+    (if (equal (xml-node-name node) name)
+	(cons node (delq nil (mapcar (lambda (nd) (xml-find-nodes-matching nd name)) (xml-node-children node))))
+      (delq nil (apply 'append (mapcar (lambda (nd) (xml-find-nodes-matching nd name)) (xml-node-children node)))))))
+
+(defun xml-print-with-preamble (xml-list)
+  (insert "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+  (xml-print xml-list)
+  (insert "\n"))
+
+(defun xml-rpc-wait-for-response (buffer)
+  (with-current-buffer buffer
+    (while (not (re-search-forward "</\\(methodResponse\\|methodCall\\)>" (point-max) t))
+      (revert-buffer t t))))
+
 (defun xml-rpc-method-call-stdout (method &rest params)
   "Call an XML-RPC method asynchronously at SERVER-URL named METHOD with \
 PARAMS as parameters. When the method returns, ASYNC-CALLBACK-FUNC will be \
@@ -14,15 +32,45 @@ called with the result as parameter."
 			   params))
          (m-func-call `((methodCall nil (methodName nil ,m-name)
                                     ,(append '(params nil) m-params)))))
-    (print m-func-call (find-file-noselect "/dev/stdout"))))
+    (with-temp-buffer
+      (xml-print-with-preamble m-func-call)
+      (insert "\n")
+      (append-to-file (point-min) (point-max) org-ikiwiki-output-file))))
 
 (defun org-ikiwiki-hook (&rest params)
-  (apply xml-rpc-method-call-stdout 'hook params))
+  (apply 'xml-rpc-method-call-stdout 'hook params))
 
-(defun org-ikiwiki-import (&rest params)
-  (org-ikiwiki-hook "type" "htmlize" "id" "org")
-  (org-ikiwiki-hook "type" "linkify" "id" "org")
-  (org-ikiwiki-hook "type" "scan" "id" "org"))
+(defun org-ikiwiki-setstate (page id key value)
+  (xml-rpc-method-call-stdout 'setstate page id key value))
+
+(defun org-ikiwiki-import (get-response-fn params)
+  (org-ikiwiki-hook "type" "htmlize" "id" "org" "call" "htmlize")
+  (funcall get-response-fn)
+;  (org-ikiwiki-hook "type" "linkify" "id" "org" "call" "linkify")
+;  (org-ikiwiki-hook "type" "scan" "id" "org" "call" "scan")
+  "1"
+  )
+
+(defun list->hash (l)
+  (let* ((ret (make-hash-table :test 'equal)))
+    (while l
+      (puthash (car l) (cadr l) ret)
+      (setq l (cddr l)))
+    ret))
+
+(defun org-ikiwiki-htmlize (get-response-fn params)
+  (let* ((content (gethash "content" params))
+	 (page (gethash "page" params))
+	 (org-export-html-preamble nil)
+	 (org-export-html-postable nil)
+	 (org-export-with-toc nil)
+	 (ret-html
+	  (with-temp-buffer
+	    (insert content)
+	    (org-mode)
+	    (org-replace-region-by-html (point-min) (point-max))
+	    (buffer-substring-no-properties (point-min) (point-max)))))
+    ret-html))
 
 (defvar org-ikiwiki-methods 
   '(("import" org-ikiwiki-import)
@@ -31,54 +79,62 @@ called with the result as parameter."
     ("scan" org-ikiwiki-scan))
   "An alist of the methods that ikiwiki can call.")
 
+(defvar org-ikiwiki-output-file nil)
+
 (defun org-ikiwiki-make-rpc-xml-get-response-fn (input-buffer)
   (lambda ()
+    (xml-rpc-wait-for-response input-buffer)
     (let* ((xml-rpc-debug 3)
-	   (with-current-buffer input-buffer
-	     (revert-buffer t t))
 	   (xml-list (xml-rpc-request-process-buffer input-buffer))
-	   (with-current-buffer input-buffer
-	     (delete-region (point-min) (point-max)))
 	   (method-or-response (car-safe (car-safe xml-list))))
+      (with-current-buffer input-buffer
+	(revert-buffer t t)
+	(delete-region (point-min) (point-max))
+	(save-buffer))
       (if (eq method-or-response 'methodResponse)
 	  (let ((valpart (cdr (cdaddr (caddar xml-list)))))
 	    (xml-rpc-xml-list-to-value valpart))
 	(error "Not a methodResponse")))))
 
-(defun org-ikiwiki-compile (input-file)
+(defun org-ikiwiki-compile (input-file output-file)
   "Runs the ikiwiki compilation process."
-  (let ((input-buffer (find-file-noselect input-file))
-	(methods org-ikiwiki-methods)
-	(org-ikiwiki-rpc-xml-get-response (org-ikiwiki-make-rpc-xml-get-response-fn input-buffer)))
+  (let* ((input-buffer (find-file-noselect input-file t))
+	 (methods org-ikiwiki-methods)
+	 (org-ikiwiki-rpc-xml-get-response (org-ikiwiki-make-rpc-xml-get-response-fn input-buffer))
+	 (auto-revert-mode nil))
+    (setq org-ikiwiki-output-file output-file)
     (while (file-exists-p input-file)
+      (xml-rpc-wait-for-response input-buffer)
       (let* ((xml-rpc-debug 3)
-	     (with-current-buffer input-buffer
-	      (revert-buffer t t))
 	     (xml-list (xml-rpc-request-process-buffer input-buffer))
-	     (with-current-buffer input-buffer
-	       (delete-region (point-min) (point-max)))
 	     (method-or-response (car-safe (car-safe xml-list))))
+	(with-current-buffer input-buffer
+	  (revert-buffer t t)
+	  (delete-region (point-min) (point-max))
+	  (save-buffer))
 	(cond
 	 ((eq method-or-response 'methodResponse)
 	  (let ((valpart (cdr (cdaddr (caddar xml-list)))))
 	    (xml-rpc-xml-list-to-value valpart)))
 	 ((eq method-or-response 'methodCall)
 	  (let* ((method-name (caddr (caddar xml-list)))
-		 (params (xml-rpc-clean (cadar (cdddar xml-list))))
+		 (params (mapcar
+			  (lambda (node)
+			    (xml-rpc-xml-list-to-value
+			     (list node)))
+			  (xml-find-nodes-matching (car xml-list) 'value)))
 		 (method (cadr (assoc method-name methods)))
-		 (result (funcall method org-ikiwiki-rpc-xml-get-response params))
-		 (m-params (mapcar '(lambda (p)
-                              `(param nil ,(car (xml-rpc-value-to-xml-list
-                                                 p))))
-				   
-				   params))
+		 (result (funcall method org-ikiwiki-rpc-xml-get-response (list->hash params)))
+		 (m-params (list `(param nil ,(car (xml-rpc-value-to-xml-list result)))))
 		 (m-response `((methodResponse nil
 					       ,(append '(params nil) m-params)))))
-	    (with-current-buffer (find-file-noselect "/dev/stdout")
-	      (xml-print m-response))))
+	    (with-current-buffer "*scratch*"
+	      (xml-print-with-preamble m-response))
+	    (with-temp-buffer 
+	      (xml-print-with-preamble m-response)
+	      (append-to-file (point-min) (point-max) org-ikiwiki-output-file))))
 	 (t
-	  (error "Not a methodCall or methodResponse")))))))
-
-(cadar (cddar '((methodCall nil (methodName nil "import") (params nil)))))
+	  (error "Not a methodCall or methodResponse")))))
+    (message "done with that file!")))
 
 (provide 'ikiwiki-org-plugin)
